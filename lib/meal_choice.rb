d5a40cad5da4a3df92601efc35cc36fb8d0c8886
Diana Ponce <dianap2341@gmail.com>
2020-01-17 01:22:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,5 @@ def meal_choice(veg1,veg2,protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
-puts veg1=broccoli
-veg2=carrots
-protein
+def meal_choice(veg1,veg2,protein = "meat")
+
